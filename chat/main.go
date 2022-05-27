21ce62800cdbaab45ef204155d929d56c7fdeaad
tb38r <tlawal86@hotmail.co.uk>
2022-05-27 16:27:48 +0100

    basic chat client functioning

--- chat/main.go
+++ chat/main.go
@@ -26,12 +26,13 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
-
-	  // root 
-	  http.Handle("/", &templateHandler{filename: "chat.html"}) 
-	  // start the web server 
-	  if err := http.ListenAndServe(":8080", nil); err != nil { 
-		log.Fatal("ListenAndServe:", err) 
-	  } 
-	} 
-
+	r := newRoom()
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+	// get the room going
+	go r.run()
+	// start the web server
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
